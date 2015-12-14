0f6c1f06d2b0bf3bbda6b7d88f6d3dbd63f420aa
dorischen21 <doris.chen21@gmail.com>
2015-12-14 11:41:49 +0800

    Update header.php
    rwd bug

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html xmlns='http://www.w4.org/1999/xhtml'>
 	<head>
-		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>
 			<?= $title ?>
 		</title>
